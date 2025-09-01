1c8196562bf76bc7bb8358e6072b1093ed45c8e9
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-09-01 19:01:07 +0300

    implemented "Application User" Created the identity DB Context and registered the service at "Infrastrucrure Registeration" Added Migration

---
+++ backend/GoEgypt Solution/Persistence/InfrastructureServicesRegisteration.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class InfrastructureServicesRegisteration
+    {
+
+        public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configurations)
+        {
+
+            services.AddDbContext<GoEgyptIdentityDbContext>(options =>
+                            options.UseSqlServer(configurations.GetConnectionString("GoEgyptIdentityConnection")));
+
+
+            services.RegisterIdentity();
+
+            return services;
+        }
+
+        private static IServiceCollection RegisterIdentity(this IServiceCollection services)
+        {
+
+            services.AddIdentityCore<ApplicationUser>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+            })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<GoEgyptIdentityDbContext>();
+
+            return services;
+        }
+
+    }
+}
