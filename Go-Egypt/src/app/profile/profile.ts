4967c5f675e9b9a3fdccd77d55d345f0ea526cdb
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-11-15 20:03:22 +0200

    fixes

--- Go-Egypt/src/app/profile/profile.ts
+++ Go-Egypt/src/app/profile/profile.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
-import { ProfileService , ProfileResponse, ProfileEditRequest} from '../services/profile';
+import { FormsModule } from '@angular/forms';
+import { ProfileService, ProfileResponse, ProfileEditRequest } from '../services/profile';
 
 @Component({
   selector: 'app-profile',
-  imports: [CommonModule, FormsModule], 
+  imports: [CommonModule, FormsModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
@@ -15,16 +15,16 @@ export class ProfileComponent implements OnInit {
 
   profileImage: string | null = null;
   isEditing = false;
-  
-  userData: ProfileResponse | null = null; 
-  
+
+  userData: ProfileResponse | null = null;
+
   editData: ProfileEditRequest = {
     displayName: '',
     about: '',
     photoLocation: ''
   };
 
-  constructor(private profileService: ProfileService) {}
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
     this.loadProfileData();
@@ -34,7 +34,7 @@ export class ProfileComponent implements OnInit {
     this.profileService.getProfileDetails().subscribe({
       next: (data: ProfileResponse) => {
         this.userData = data;
-        this.profileImage = data.profilePicture; 
+        this.profileImage = data.profilePicture;
       },
       error: (err) => console.error('Error loading profile data:', err)
     });
@@ -47,19 +47,11 @@ export class ProfileComponent implements OnInit {
     this.profileService.uploadProfileImage(file).subscribe({
       next: (response) => {
         const newImageUrl = response.newUrl;
-        this.profileImage = newImageUrl; 
-        this.userData!.profilePicture = newImageUrl; 
 
-        const editRequest: ProfileEditRequest = {
-          displayName: this.userData!.displayName,
-          about: this.userData!.about,
-          photoLocation: newImageUrl
-        };
+        this.profileImage = newImageUrl;
 
-        this.profileService.editProfile(editRequest).subscribe({
-          next: () => console.log('Profile picture updated successfully!'),
-          error: (err) => console.error('Error saving new profile picture URL:', err)
-        });
+        this.editData.photoLocation = newImageUrl;
+
       },
       error: (err) => console.error('Error uploading image:', err)
     });
@@ -72,13 +64,14 @@ export class ProfileComponent implements OnInit {
       this.editData = {
         displayName: this.userData.displayName,
         about: this.userData.about,
-        photoLocation: this.userData.profilePicture
+        photoLocation: this.userData.profilePicture 
       };
     }
   }
 
   cancelEdit(): void {
     this.isEditing = false;
+
     if (this.userData) {
       this.profileImage = this.userData.profilePicture;
     }
@@ -90,7 +83,7 @@ export class ProfileComponent implements OnInit {
     const request: ProfileEditRequest = {
       displayName: this.editData.displayName,
       about: this.editData.about,
-      photoLocation: this.userData.profilePicture 
+      photoLocation: this.editData.photoLocation
     };
 
     this.profileService.editProfile(request).subscribe({
@@ -98,6 +91,8 @@ export class ProfileComponent implements OnInit {
         if (success) {
           this.userData!.displayName = this.editData.displayName;
           this.userData!.about = this.editData.about;
+          this.userData!.profilePicture = this.editData.photoLocation;
+
           this.isEditing = false;
         } else {
           console.error('Failed to save profile (backend returned false)');
